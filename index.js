1b22d70959c6a1e55feefde2b5a864995e0ad180
git_dave <ctchapman@gmail.com>
2018-04-29 13:49:49 -0700

    added get/post with SQL and pgsql

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ pgp.pg.defaults.ssl = true;
 //Create connection to Heroku Database
 let db;
 //Uncomment next line and change the string to your DATABASE_URL
-//db = pgp('<SET ME TO Result of `heroku config:get DATABASE_URL -a tcss450`');
+db = pgp('postgres://nhalvucsuqwpfk:7cc02a001a0dec4a123302faabb938c9530ec5642eb9378092a7ba91802ae1d8@ec2-54-221-192-231.compute-1.amazonaws.com:5432/dv4igi32q700c');
 
-//if(!db) {
-//    console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
-//    process.exit(1);
-//}
+if(!db) {
+   console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
+   process.exit(1);
+}
 
 
 /**
@@ -82,6 +82,7 @@ app.get("/params", (req, res) => {
         message: "Hello, " + req.query['name'] + "!"
         });
     });
+
 app.post("/params", (req, res) => {
     res.send({
     //req.query is a reference to arguments in the POST body
@@ -94,6 +95,50 @@ app.get("/wait", (req, res) => {
         message: "Thanks for waiting"
         });
     }, 1000);
+});
+
+app.post("/demosql", (req, res) => {
+    var name = req.body['name'];
+    if (name) {
+        let params = [name];
+        db.none("INSERT INTO DEMO(Text) VALUES ($1)", params)
+        .then(() => {
+            //We successfully added the name, let the user know
+            res.send({
+                success: true
+            });
+        }).catch((err) => {
+            //log the error
+            console.log(err);
+            res.send({
+                success: false,
+                error: err
+            });
+        });
+    } else {
+        res.send({
+            success: false,
+            input: req.body,
+            error: "Missing required information"
+        });
+    }
+});
+
+app.get("/demosql", (req, res) => {
+    db.manyOrNone('SELECT Text FROM Demo')
+    //If successful, run function passed into .then()
+    .then((data) => {
+        res.send({
+            success: true,
+            names: data
+        });
+    }).catch((error) => {
+        console.log(error);
+        res.send({
+            success: false,
+            error: error
+        })
+    });
 });
 
 /*
