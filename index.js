9b7da7247b03c2895caf0993fdb643fe33bee899
Igor Kalezic <igor2010@live.com>
2018-05-02 14:18:06 -0700

    igor branch

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const crypto = require("crypto");
 //    console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
 //    process.exit(1);
 // }
+var chat = require('.\routes\chat.js');
+app.use('/chat', chat);
 
 var login = require('./routes/login.js');
 app.use('/login', login);
