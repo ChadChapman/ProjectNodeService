9d2fa1a64cdbaf6b59c80883a3960615cc514829
Igor Kalezic <igor2010@live.com>
2018-05-02 16:55:54 -0700

    All inserts

--- routes/chat.js
+++ routes/chat.js
@@ -45,7 +45,6 @@ router.post("/", (req, res) => {
         let chid = [chatID];
         db.none("INSERT INTO Chats(Name) VALUES ('" + uName + "')")
         db.none("INSERT INTO ChatMembers(MemberID, ChatID) VALUES (" + memid + ", " + chid + ")")
-        db.none("INSERT INTO Messages(Message) VALUES (" + "'" + msg + "'"+ ")")
         .then(() => {
             //We successfully addevd the name, let the user know
             res.send({
